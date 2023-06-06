94cca45642b19c5d77089ea83e4421a063e99aa3
bunjamat <bunjamat@gmail.com>
2023-06-06 17:42:19 +0700

    update 1

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,10 +2,12 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -18,30 +20,32 @@ const Nav = () => {
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <Link href={"/"} className="flex gap-2 flex-center">
+      <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src={"/assets/images/logo.svg"}
+          src="/assets/images/logo.svg"
           alt="logo"
           width={30}
           height={30}
           className="object-contain"
-        ></Image>
+        />
         <p className="logo_text">Promptopia</p>
       </Link>
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
-            <Link href={"/create-prompt"} className="black_btn">
+            <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
+
             <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
+
             <Link href="/profile">
               <Image
-                src={"assets/images/logo.svg"}
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -70,15 +74,15 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src={"assets/images/logo.svg"}
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
               alt="profile"
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
